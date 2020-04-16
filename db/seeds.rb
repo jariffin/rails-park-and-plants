00a84c8c7a607fd7c1745f8a6a5ddfc50b8bc502
Juliana Ariffin <juliana@fastmail.com>
2020-04-16 16:53:38 +0200

    models, controllers, routes and garden action

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Destroy all"
+Garden.destroy_all
+
+puts "Creating gardens"
+garden = Garden.create!(
+    name: "Eden",
+    picture: "https://unsplash.com/photos/wzcw_-sn0fk"
+  )
+
+garden = Garden.create!(
+    name: "Wonderland",
+    picture: "https://unsplash.com/photos/ycAzNfHtcWk"
+  )
+
+puts "#{garden.name}"
